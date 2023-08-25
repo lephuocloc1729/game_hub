b3a1a68c250ef0c1db6b618c41edb2893942bc51
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-25 20:27:21 +0700

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <main className="grid grid-areas-sm lg:grid-areas-lg">
-      <section className="nav">
+      <nav className="nav">
         <Navbar />
-      </section>
+      </nav>
       <section className="hidden aside lg:block">aside</section>
       <section className="main">main</section>
       <section></section>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,9 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <img src="" alt="Logo" />
+    </>
+  );
 };
 
 export default Navbar;
