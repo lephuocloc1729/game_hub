32a9d330543848a13e04981bb56cfd7c44e5c8ce
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-26 11:49:45 +0700

    Feature: Generate platform icons
    -Create PlatformIconList component to display platforms' icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,17 @@
 import { Game } from "../services/fetchGames";
+import PlatformIconList from "./PlatformIconList";
 const GameCard = ({ game }: { game: Game }) => {
   return (
-    <div className="rounded-2xl overflow-hidden bg-slate-700">
+    <div className="rounded-2xl overflow-hidden bg-slate-200 dark:bg-slate-700">
       <img src={game.background_image} alt="" />
-      <h2 className="bg-slate-700 text-3xl py-4 px-6 flex">{game.name}</h2>
+      <div className="py-4 px-6">
+        <h2 className="dark:bg-slate-700 dark:text-white text-3xl mb-4">
+          {game.name}
+        </h2>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </div>
     </div>
   );
 };
--- src/services/fetchGames.tsx
+++ src/services/fetchGames.tsx
@@ -1,8 +1,16 @@
 import apiClient from "./api-client";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
