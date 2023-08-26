a2865eec68de1f68405efb6ff56d1bda6393dd92
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-26 12:23:50 +0700

    get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,11 @@
 import { Game } from "../services/fetchGames";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 const GameCard = ({ game }: { game: Game }) => {
   return (
     <div className="rounded-2xl overflow-hidden bg-slate-200 dark:bg-slate-700 drop-shadow-xl">
-      <img src={game.background_image} alt="" />
+      <img src={getCroppedImageUrl(game.background_image)} alt="" />
       <div className="py-4 px-6">
         <h2 className="dark:bg-slate-700 dark:text-white text-3xl mb-4">
           {game.name}
