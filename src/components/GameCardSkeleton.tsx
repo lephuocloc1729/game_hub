2fe594b9f96f0672470b1b319830be3b39f25124
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-26 13:08:37 +0700

    adjust dark mode and light mode for loading skeletons

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,9 +1,9 @@
 const GameCardSkeleton = () => {
   return (
-    <div className="flex flex-col gap-6 w-full bg-slate-700 rounded-2xl h-80 overflow-hidden animate-pulse">
-      <div className="w-full bg-slate-600 h-[70%]"></div>
-      <div className="w-4/5 bg-slate-600 h-3 rounded-3xl ml-4 animate-pulse"></div>
-      <div className="w-2/5 bg-slate-600 h-3 rounded-3xl ml-4 animate-pulse"></div>
+    <div className="flex flex-col gap-6 w-full bg-slate-200 dark:bg-slate-700 rounded-2xl h-80 overflow-hidden drop-shadow-xl">
+      <div className="w-full bg-slate-300 dark:bg-slate-600 h-[70%] animate-pulse"></div>
+      <div className="w-4/5 bg-slate-300 dark:bg-slate-600 h-3 rounded-3xl ml-4 animate-pulse"></div>
+      <div className="w-2/5 bg-slate-300 dark:bg-slate-600 h-3 rounded-3xl ml-4 animate-pulse"></div>
     </div>
   );
 };
