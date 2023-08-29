b6a19b74350632990fb79d6784e266dcd857edb2
PhuocLoc <lephuocloc1729@gmail.com>
2023-08-29 07:47:39 +0700

    use Link element to create Logo
    allow users to return home by clicking on the logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useGameQuery from "../GameQueryStore";
 import SearchInput from "./SearchInput";
 const Navbar = () => {
@@ -5,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between pt-4 items-center px-4 cursor-pointer nav pb-5 bg-dark-blue text-white">
       <h1 className="text-2xl font-bold" onClick={reset}>
-        RAWG
+        <Link to="/">RAWG</Link>
       </h1>
       <SearchInput />
       <div className="w-10 h-10 rounded-full bg-blue-500 text-white flex items-center justify-center font-bold shrink-0">
